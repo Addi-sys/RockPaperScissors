d35dad6e8a45545d7c51e8bb2303ec377b3e21bc
Addi <coxonmarc1@gmail.com>
2020-07-21 10:03:18 +0700

    fix for netlify

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import ChoiceCard from './components/ChoiceCard.js'
 
-const choices = {
-  rock:
-    "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
-  paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
-  scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
-};
+// const choices = {
+//   rock:
+//     "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
+//   paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
+//   scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
+// };
 
 
 function App() {
 
-  const [prompt, setGamePrompt] = useState("Start")
+  // const [prompt, setGamePrompt] = useState("Start")
 
   return (
     <div className="App">
@@ -20,7 +20,7 @@ function App() {
         <div className="row mb-3">
           <div className="col-md-8 themed-grid-col">
             <ChoiceCard title="Computer" />
-            <h1>{prompt}</h1>
+            {/* <h1>{prompt}</h1> */}
             <ChoiceCard title="You" />
           </div>
         </div>
