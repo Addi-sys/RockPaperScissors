7694dede50d8338f1dcded9aa4ce66de4174f8f6
Addi <coxonmarc1@gmail.com>
2020-07-21 09:52:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ChoiceCard from './components/ChoiceCard.js'
+
+const choices = {
+  rock:
+    "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
+  paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
+  scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
+};
+
 
 function App() {
+
+  const [prompt, setGamePrompt] = useState("Start")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row mb-3">
+          <div className="col-md-8 themed-grid-col">
+            <ChoiceCard title="Computer" />
+            <h1>{prompt}</h1>
+            <ChoiceCard title="You" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// winner={true} imgURL={choices.paper}
+// winner={false} imgURL={choices.rock} 
